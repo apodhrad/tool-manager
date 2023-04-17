604e84f07ceb90547b200e003f13a5ba386b89c5
Andrej Podhradsky <apodhrad@redhat.com>
2023-04-17 14:10:45 +0200

    Add table tests in root.go
    Signed-off-by: Andrej Podhradsky <apodhrad@redhat.com>

--- cmd/root_test.go
+++ cmd/root_test.go
@@ -1,7 +1,33 @@
 package cmd
 
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
 func setup() {
 }
 
 func teardown() {
+}
+
+func TestEmptyTable(t *testing.T) {
+	expected := `HEADERA  HEADERB  HEADERC  
+`
+	tbl := NewTable("HeaderA", "HeaderB", "HeaderC")
+	actual := TableToString(tbl)
+	assert.Equal(t, expected, actual)
+}
+
+func TestNonEmptyTable(t *testing.T) {
+	expected := `HEADERA          HEADERB  HEADERC  
+ValueA1          ValueB1  ValueC1  
+VeryLongValueA2  ValueB2  ValueC2  
+`
+	tbl := NewTable("HeaderA", "HeaderB", "HeaderC")
+	tbl.AddRow("ValueA1", "ValueB1", "ValueC1")
+	tbl.AddRow("VeryLongValueA2", "ValueB2", "ValueC2")
+	actual := TableToString(tbl)
+	assert.Equal(t, expected, actual)
 }
